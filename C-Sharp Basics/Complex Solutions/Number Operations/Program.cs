8bdb1f16c0091d0e13ac85893cf7531ed8c7baca
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:47:49 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Number Operations/Program.cs
+++ C-Sharp Basics/Complex Solutions/Number Operations/Program.cs
@@ -1,1 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            string oper = Console.ReadLine();
+
+            double result = 0.00;
+            string output = string.Empty;
+
+            if (n2 == 0 && (oper == "/" || oper == "%"))
+            {
+                output = string.Format($"Cannot divide {n1} by zero");
+            }
+            else if (oper == "/")
+            {
+                result = n1 / n2;
+                output = string.Format($"{n1} {oper} {n2} = {result:f2}");
+            }
+            else if (oper == "%")
+            {
+                result = n1 % n2;
+                output = string.Format($"{n1} {oper} {n2} = {result}");
+            }
+            else
+            {
+                if (oper == "+")
+                {
+                    result = n1 + n2;
+                }
+                else if (oper == "-")
+                {
+                    result = n1 - n2;
+                }
+                else if (oper == "*")
+                {
+                    result = n1 * n2;
+                }
+                if (result % 2 == 0)
+                {
+                    output = string.Format($"{n1} {oper} {n2} = {result} - even");
+
+                }
+                else
+                {
+                    output = string.Format($"{n1} {oper} {n2} = {result} - odd");
+
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
 
