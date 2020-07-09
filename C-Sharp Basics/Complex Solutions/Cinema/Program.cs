8a3fd619fa857628c26ee01dd3c5d74177ebe32f
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:37:00 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Cinema/Program.cs
+++ C-Sharp Basics/Complex Solutions/Cinema/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine().ToLower();
+            int row = int.Parse(Console.ReadLine());
+            int column = int.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (projection == "premiere")
+            {
+                price = 12.00;
+            }
+            else if (projection == "normal")
+            {
+                price = 7.50;
+            }
+            else if (projection == "discount")
+            {
+                price = 5.00;
+            }
+            double income = row * column * price;
+            Console.WriteLine($"{income:f2}");
+        }
+    }
+}
