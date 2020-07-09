f8f5a0cea9853e3577ff78a5615c86683864d188
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:07:21 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Circle/Program.cs
+++ C-Sharp Basics/Simple Solutions/Circle/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            double perimeter = 2 * Math.PI * r;
+            Console.WriteLine("area = " + area);
+            Console.WriteLine("perimeter = " + perimeter);
+        }
+    }
+}
