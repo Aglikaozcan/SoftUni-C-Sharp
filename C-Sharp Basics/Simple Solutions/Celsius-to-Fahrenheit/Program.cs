3ce3b936b78f047805aeb988405fc5a3a3905649
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:06:19 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Celsius-to-Fahrenheit/Program.cs
+++ C-Sharp Basics/Simple Solutions/Celsius-to-Fahrenheit/Program.cs
@@ -1,1 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Celsius_to_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("Celsius = ");
+            double c = double.Parse(Console.ReadLine());
+            double f = c * 1.8 + 32;
+            Console.WriteLine("Fahrenheit = {0}", Math.Round(f,2));
+        }
+    }
+}
