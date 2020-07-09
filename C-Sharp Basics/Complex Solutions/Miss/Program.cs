e1940bfdd1423f7778ce563f6e43cd53f681d930
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:47:03 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Miss/Program.cs
+++ C-Sharp Basics/Complex Solutions/Miss/Program.cs
@@ -1,1 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Miss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (gender == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
