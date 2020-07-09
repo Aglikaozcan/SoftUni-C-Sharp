bd809803ea161e91ff5807c043452ffdb285b9e7
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:12:43 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Square-area/Program.cs
+++ C-Sharp Basics/Simple Solutions/Square-area/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Square_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            int Square_area = a * a;
+            Console.Write("Square area = ");
+            Console.WriteLine(Square_area);
+        }
+    }
+}
