32d28ade8bb9871ea6b2d2b615e12712f7d29667
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:11:02 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Inch-to-cm/Program.cs
+++ C-Sharp Basics/Simple Solutions/Inch-to-cm/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Inch_to_cm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inches = ");
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.Write("centimeters = ");
+            Console.WriteLine(centimeters);
+        }
+    }
+}
