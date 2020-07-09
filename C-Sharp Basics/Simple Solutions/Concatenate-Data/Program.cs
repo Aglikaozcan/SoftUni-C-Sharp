52f98661b2542319ae7ee9315a55f407c10da50b
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:08:24 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Concatenate-Data/Program.cs
+++ C-Sharp Basics/Simple Solutions/Concatenate-Data/Program.cs
@@ -1,1 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Concatenate_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First name = ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Last name = ");
+            string LastName = Console.ReadLine();
+            Console.Write("Age = ");
+            int Age = int.Parse(Console.ReadLine());
+            Console.Write("Town = ");
+            string Town = Console.ReadLine();
+            Console.WriteLine("You are " + FirstName + " " + LastName + ", " + "a " + Age + "-years old person from " + Town + ".");
+        }
+    }
+}
