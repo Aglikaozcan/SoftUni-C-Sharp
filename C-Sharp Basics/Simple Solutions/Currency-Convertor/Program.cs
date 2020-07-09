687fd644dd989e6c1a91d10cde6b7873bc35d70a
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-09 20:09:15 +0300

    Add files via upload

--- C-Sharp Basics/Simple Solutions/Currency-Convertor/Program.cs
+++ C-Sharp Basics/Simple Solutions/Currency-Convertor/Program.cs
@@ -1,1 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Currency_Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("Amount = ");
+           // Console.Write("Входна валута = ");
+            double amount = double.Parse(Console.ReadLine());
+            double entryCurr = double.Parse(Console.ReadLine());
+            double exitCurr = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
