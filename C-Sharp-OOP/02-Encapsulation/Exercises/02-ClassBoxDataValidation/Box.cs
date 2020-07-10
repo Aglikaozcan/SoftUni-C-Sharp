d66067412d2db66941036c7ef805ab6e36556fbd
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:16:55 +0300

    Add files via upload

---
+++ C-Sharp-OOP/02-Encapsulation/Exercises/02-ClassBoxDataValidation/Box.cs
@@ -0,0 +1,80 @@
+﻿namespace P02.ClassBoxDataValidation
+{
+    using System;
+
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Length));
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {                
+                this.ValidateParameter(value, nameof(this.Width));
+                this.width = value;
+            }
+        }       
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ValidateParameter(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        private void ValidateParameter(double value, string parameterName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{parameterName} cannot be zero or negative.");
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            double surfaceArea = 2 *
+                (this.length * this.width
+                + this.length * this.height
+                + this.width * this.height);
+
+            return surfaceArea;
+        }
+
+        public double GetVolume()
+        {
+            double volume = this.length * this.width * this.height;
+
+            return volume;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * this.height * (this.length + this.width);
+
+            return lateralSurfaceArea;
+        }        
+    }
+}
