6544278aeffc1392b9becd6a83e2a44c755cfdce
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:08:11 +0300

    Add files via upload

---
+++ C-Sharp-OOP/02-Encapsulation/Lab/02-SalaryIncrease/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName => this.firstName;
+
+        public string LastName => this.lastName;
+
+        public int Age => this.age;
+
+        public decimal Salary => this.salary; 
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age > 30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.".ToString();
+        }
+    }
+}
--- C-Sharp-OOP/02-Encapsulation/Lab/02-SalaryIncrease/StartUp.cs
+++ C-Sharp-OOP/02-Encapsulation/Lab/02-SalaryIncrease/StartUp.cs
@@ -1,1 +1,31 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var person = new Person(
+                    input[0], 
+                    input[1], 
+                    int.Parse(input[2]), 
+                    decimal.Parse(input[3]));
+                persons.Add(person);
+            }
+
+            var percentage = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
