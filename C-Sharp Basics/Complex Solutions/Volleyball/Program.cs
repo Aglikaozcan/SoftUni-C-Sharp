e150c5a72e617e36fd65c29be47c2d3891078266
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:47:22 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Volleyball/Program.cs
+++ C-Sharp Basics/Complex Solutions/Volleyball/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            int holidays = int.Parse(Console.ReadLine());
+            int homeWeekend = int.Parse(Console.ReadLine());
+
+            int weekendSof = 48 - homeWeekend;
+            double playSat = weekendSof * 3.0 / 4;
+            double playHoliday = holidays * 2.0 / 3;
+            double totalGames = playSat + homeWeekend + playHoliday;
+
+            if (year == "leap")
+            {
+                double extraLeap = totalGames * 0.15;
+                double games = totalGames + extraLeap;
+                Console.WriteLine(Math.Floor(games));
+            }
+            else if (year == "normal")
+            {
+                Console.WriteLine(Math.Floor(totalGames));
+            }
+        }
+    }
+}
