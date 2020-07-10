d0b8dd4ba1e6582c8ff9a34cfdcb232ce61e2c94
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:24:24 +0300

    Add files via upload

--- C-Sharp-OOP/02-Encapsulation/Exercises/06-FootballTeamGenerator/Program.cs
+++ C-Sharp-OOP/02-Encapsulation/Exercises/06-FootballTeamGenerator/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace P06.FootballTeamGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C-Sharp-OOP/02-Encapsulation/Exercises/06-FootballTeamGenerator/Team.cs
@@ -0,0 +1,58 @@
+﻿namespace P06.FootballTeamGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Team
+    {
+        private string name;
+        private List<Player> players;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            this.players.Add(player);
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            int playerIndex = this.players.FindIndex(x => x.Name == playerName);
+
+            if (playerIndex == -1)
+            {
+                throw new ArgumentException($"Player {playerName} is not in {this.Name} team.");
+            }
+
+            players.RemoveAt(playerIndex);
+        }
+
+        public double GetRating()
+        {
+            if (this.players.Count > 0)
+            {
+                return (int)Math.Round(this.players.Average(x => x.OverallSkillLevel());
+            }
+            return this.players.
+        }
+    }
+}
