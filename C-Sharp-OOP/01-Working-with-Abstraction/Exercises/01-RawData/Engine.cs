4f2e5d903ad067c6e970eca36d2980415056ca28
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:52:11 +0300

    Add files via upload

---
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/01-RawData/Engine.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_RawData
+{
+    public class Engine
+    {
+        public Engine(int speed, int power)
+        {
+            this.Speed = speed;
+            this.Power = power;
+        }
+
+        public int Power { get; private set; }
+
+        public int Speed { get; private set; }
+    }
+}
