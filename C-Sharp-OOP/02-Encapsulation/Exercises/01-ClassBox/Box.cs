7c4e66c89b8ca0972b1e032597b90035222ac56a
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:12:32 +0300

    Add files via upload

---
+++ C-Sharp-OOP/02-Encapsulation/Exercises/01-ClassBox/Box.cs
@@ -0,0 +1,53 @@
+﻿namespace P01.ClassBox
+{
+    using System.Text;
+
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetSurfaceArea()
+        {
+            double surfaceArea = 2 * 
+                (this.length * this.width
+                + this.length * this.height
+                + this.width * this.height);
+
+            return surfaceArea;
+        }
+
+        public double GetVolume()
+        {
+            double volume = this.length * this.width * this.height;
+
+            return volume;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * this.height * (this.length + this.width);
+
+            return lateralSurfaceArea;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {this.GetSurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():f2}");
+            sb.Append($"Volume - {this.GetVolume():f2}");
+
+            return sb.ToString();
+        }
+    }
+}
--- C-Sharp-OOP/02-Encapsulation/Exercises/01-ClassBox/Program.cs
+++ C-Sharp-OOP/02-Encapsulation/Exercises/01-ClassBox/Program.cs
@@ -1,1 +1,18 @@
+﻿namespace P01.ClassBox
+{
+    using System;
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine(box);
+        }
+    }
+}
