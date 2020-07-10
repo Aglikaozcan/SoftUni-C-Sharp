7249f06cb39ee243a24c561c46dde4f23d448082
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:42:06 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Retirement/Program.cs
+++ C-Sharp Basics/Complex Solutions/Retirement/Program.cs
@@ -1,1 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Retirement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gender = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int staj = int.Parse(Console.ReadLine());
+
+            int retirementAge = 0;
+            int retirementStaj = 0;
+            bool isGender = gender == "female" || gender == "male";
+
+            if (isGender)
+            {
+                if (gender == "female")
+                {
+                    retirementAge = 61;
+                    retirementStaj = 35;
+                }
+                else if (gender == "male")
+                {
+                    retirementAge = 64;
+                    retirementStaj = 38;
+                }
+
+                if (age >= retirementAge && staj >= retirementStaj)
+                {
+                    Console.WriteLine($"Ready to retire at {age} and {staj} years of experience!");
+                }
+                else if (age < retirementAge && staj >= retirementStaj)
+                {
+                    Console.WriteLine($"Worked enough, but not old enough. Years left to retirement: {retirementAge - age}.");
+                }
+                else if (age >= retirementAge && staj < retirementStaj)
+                {
+                    Console.WriteLine($"Old enough, but haven't worked enough. Work experience left to retirement: {retirementStaj - staj}.");
+                }
+                else if (age < retirementAge && staj < retirementStaj)
+                {
+                    Console.WriteLine($"Too early. Years left to retirement: {retirementAge - age}. Work experience left to retirement: {retirementStaj - staj}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+    }
+}
