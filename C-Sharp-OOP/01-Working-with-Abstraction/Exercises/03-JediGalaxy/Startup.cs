7f3d8dd8cf5460c293579553d2664787ae817b30
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:55:44 +0300

    Add files via upload

--- C-Sharp-OOP/01-Working-with-Abstraction/Exercises/03-JediGalaxy/Startup.cs
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/03-JediGalaxy/Startup.cs
@@ -1,1 +1,74 @@
+﻿namespace P03_JediGalaxy
+{
+    using System;
+    using System.Linq;
 
+    public class Startup
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(new string[] { " " }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            var galaxy = new Galaxy(rows, cols);
+
+            string command = Console.ReadLine();
+            long ivoPoints = 0;
+
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoCoordinates = command
+                    .Split(new string[] { " " }
+                    , StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var ivo = new Player();
+                ivo.Row = ivoCoordinates[0];
+                ivo.Col = ivoCoordinates[1];
+
+                int[] evilCoordinates = Console.ReadLine()
+                    .Split(new string[] { " " }
+                    , StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var evil = new Player();
+                evil.Row = evilCoordinates[0];
+                evil.Col = evilCoordinates[1];
+
+                while (evil.Row >= 0 && evil.Col >= 0)
+                {
+                    if (galaxy.IsInside(evil.Row, evil.Col))
+                    {
+                        galaxy.Matrix[evil.Row, evil.Col] = 0;
+                    }
+
+                    evil.Row--;
+                    evil.Col--;
+                }
+
+                while (ivo.Row >= 0 && ivo.Col < galaxy.Matrix.GetLength(1))
+                {
+                    if (galaxy.IsInside(ivo.Row, ivo.Col))
+                    {
+                        ivoPoints += galaxy.Matrix[ivo.Row, ivo.Col];
+                    }
+
+                    ivo.Col++;
+                    ivo.Row--;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(ivoPoints);
+        }
+    }
+}
