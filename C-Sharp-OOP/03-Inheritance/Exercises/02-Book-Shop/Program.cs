70689ea6d2cf00002c7598fdcff2152cd8ea7b58
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:59:40 +0300

    Add files via upload

---
+++ C-Sharp-OOP/03-Inheritance/Exercises/02-Book-Shop/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P02.BookShop
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string author = Console.ReadLine();
+                string title = Console.ReadLine();
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Book book = new Book(author, title, price);
+                GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+                Console.WriteLine(book);
+                Console.WriteLine();
+                Console.WriteLine(goldenEditionBook);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
