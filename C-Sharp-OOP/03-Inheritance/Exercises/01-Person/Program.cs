e98cbd4788f05fd0101b8e4488e7b2b60ea2af68
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:58:45 +0300

    Add files via upload

--- C-Sharp-OOP/03-Inheritance/Exercises/01-Person/Program.cs
+++ C-Sharp-OOP/03-Inheritance/Exercises/01-Person/Program.cs
@@ -1,1 +1,25 @@
+﻿namespace P01.Person
+{
+    using System;
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Person person = new Person(name, age);
+                Child child = new Child(name, age);
+
+                Console.WriteLine(person.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
