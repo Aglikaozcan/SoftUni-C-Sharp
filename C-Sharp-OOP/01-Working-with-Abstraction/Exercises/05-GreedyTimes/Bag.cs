e419251d9053b9428d5c1ebdc24eaba0bd7e8e74
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:57:51 +0300

    Add files via upload

---
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/05-GreedyTimes/Bag.cs
@@ -0,0 +1,108 @@
+﻿namespace P05_GreedyTimes2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Bag
+    {
+        private long capacity;
+        private long currentCapacity;
+        private long gold;
+        private Dictionary<string, long> gems;
+        private long totalGems;
+        private Dictionary<string, long> cash;
+        private long totalCash;
+
+        public Bag(long capacity)
+        {
+            this.capacity = capacity;
+            this.currentCapacity = 0;
+            this.gold = 0;
+            this.gems = new Dictionary<string, long>();
+            this.totalGems = 0;
+            this.cash = new Dictionary<string, long>();
+            this.totalCash = 0;
+        }
+
+        public void AddCash(string item, long quantity)
+        {
+            if (this.HasFreeCapacity(quantity) && this.totalGems >= this.totalCash + quantity)
+            {
+                if (!this.cash.ContainsKey(item))
+                {
+                    this.cash.Add(item, 0);
+                }
+                this.cash[item] += quantity;
+
+                this.currentCapacity += quantity;
+                this.totalCash += quantity;
+            }
+        }
+
+        public void AddGold(long quantity)
+        {
+            if (this.HasFreeCapacity(quantity))
+            {
+                this.gold += quantity;
+                this.currentCapacity += quantity;
+            }
+        }
+
+        public void AddGems(string item, long quantity)
+        {
+            if (this.HasFreeCapacity(quantity) && this.gold >= this.totalGems + quantity)
+            {
+                if (!this.gems.ContainsKey(item))
+                {
+                    this.gems.Add(item, 0);
+                }
+                this.gems[item] += quantity;
+
+                this.currentCapacity += quantity;
+                this.totalGems += quantity;
+            }
+        }
+
+        private bool HasFreeCapacity(long quantity)
+        {
+            return this.currentCapacity + quantity <= this.capacity;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.gold > 0)
+            {
+                sb.AppendLine($"<Gold> ${this.gold}");
+                sb.AppendLine($"##Gold - {this.gold}");
+            }
+                                    
+            if (this.gems.Any())
+            {
+                var orderedGems = gems
+                .OrderByDescending(x => x.Key)
+                .ThenBy(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+                sb.AppendLine($"<Gem> ${this.totalGems}");
+                sb.AppendLine(string.Join(Environment.NewLine, orderedGems.Select(x => $"##{x.Key} - {x.Value}")));
+            }
+
+            if (this.cash.Any())
+            {
+                var orderedCash = cash
+                .OrderByDescending(x => x.Key)
+                .ThenBy(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+                sb.AppendLine($"<Cash> ${this.totalCash}");
+                sb.Append(string.Join(Environment.NewLine, orderedCash.Select(x => $"##{x.Key} - {x.Value}")));
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- C-Sharp-OOP/01-Working-with-Abstraction/Exercises/05-GreedyTimes/Program.cs
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/05-GreedyTimes/Program.cs
@@ -1,1 +1,38 @@
+﻿namespace P05_GreedyTimes2
+{
+    using System;
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long capacity = long.Parse(Console.ReadLine());
+
+            Bag bag = new Bag(capacity);
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < input.Length; i+=2)
+            {
+                string item = input[i];
+                long quantity = long.Parse(input[i + 1]);
+
+                if (item.Length == 3)
+                {
+                    bag.AddCash(item, quantity);
+                }
+                else if (item.ToLower().EndsWith("gem") && item.Length > 3)
+                {
+                    bag.AddGems(item, quantity);
+                }
+                else if (item.ToLower() == "gold")
+                {
+                    bag.AddGold(quantity);
+                }
+            }
+
+            Console.WriteLine(bag.ToString());
+        }
+    }
+}
