f8703ff4760d784dc1215f4691fbfe2b5ef43cc5
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:37:11 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Point in Rectangle/Program.cs
+++ C-Sharp Basics/Complex Solutions/Point in Rectangle/Program.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace point_in_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            bool insideX = x >= x1 && x <= x2;
+            bool insideY = y >= y1 && y <= y2;
+
+            if (insideX && insideY)
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Outside");
+            }
+        }
+    }
+}
