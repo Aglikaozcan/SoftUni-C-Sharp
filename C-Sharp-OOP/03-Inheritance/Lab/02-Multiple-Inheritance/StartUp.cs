31eecc2b0e8572bc9d70fbd277f8b4cbe6b087c1
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 11:54:28 +0300

    Add files via upload

--- C-Sharp-OOP/03-Inheritance/Lab/02-Multiple-Inheritance/StartUp.cs
+++ C-Sharp-OOP/03-Inheritance/Lab/02-Multiple-Inheritance/StartUp.cs
@@ -1,1 +1,18 @@
+﻿using System;
 
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+            Console.WriteLine(puppy.Weep());
+        }
+    }
+}
