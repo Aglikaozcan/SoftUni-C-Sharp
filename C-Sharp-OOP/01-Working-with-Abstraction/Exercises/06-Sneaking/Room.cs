ec93d4d727e9681ddffdc745030e1950c2c47614
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:58:40 +0300

    Add files via upload

---
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/06-Sneaking/Room.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P06_Sneaking
+{
+    public class Room
+    {
+        public Room(int rows)
+        {
+            this.Matrix = new char[rows][];
+            this.InitializeRoom();
+        }
+
+        public char[][] Matrix { get; set; }
+
+        public int SamRow { get; set; }
+
+        public int SamCol { get; set; }
+
+        private void InitializeRoom()
+        {
+            for (int row = 0; row < this.Matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine()
+                    .ToCharArray();
+
+                this.Matrix[row] = new char[input.Length];
+
+                for (int col = 0; col < input.Length; col++)
+                {
+                    this.Matrix[row][col] = input[col];
+                }
+            }
+        }
+
+        public void FindSam()
+        {            
+            for (int row = 0; row < this.Matrix.GetLength(0); row++)
+            {
+                this.Matrix[row] = Console.ReadLine()
+                    .ToCharArray();
+
+                for (int col = 0; col < this.Matrix[row].Length; col++)
+                {
+                    if (this.Matrix[row][col] == 'S')
+                    {
+                        SamRow = row;
+                        SamCol = col;
+                    }
+                }
+            }
+        }
+
+        public void MoveEnemies()
+        {
+            for (int row = 0; row < this.Matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < this.Matrix[row].Length; col++)
+                {
+                    if (this.Matrix[row][col] == 'd')
+                    {
+                        if (col == 0)
+                        {
+                            this.Matrix[row][col] = 'b';
+                        }
+
+                        else
+                        {
+                            this.Matrix[row][col] = '.';
+                            this.Matrix[row][col - 1] = 'd';
+                        }
+
+                        continue;
+                    }
+
+                    if (this.Matrix[row][col] == 'b')
+                    {
+                        if (col == this.Matrix[row].Length - 1)
+                        {
+                            this.Matrix[row][col] = 'd';
+                        }
+
+                        else
+                        {
+                            this.Matrix[row][col] = '.';
+                            this.Matrix[row][col + 1] = 'b';
+                            col++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool CaughtByEnemy()
+        {
+            for (int col = 0; col < SamCol; col++)
+            {
+                if (this.Matrix[SamRow][col] == 'b')
+                {
+                    return true;
+                }
+            }
+
+            for (int col = SamCol + 1; col < this.Matrix[SamRow].Length; col++)
+            {
+                if (this.Matrix[SamRow][col] == 'd')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void PrintField()
+        {
+            foreach (var row in this.Matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        public bool CheckForNikoladze()
+        {
+            for (int col = 0; col < this.Matrix[SamRow].Length; col++)
+            {
+                if (Matrix[SamRow][col] == 'N')
+                {
+                    Matrix[SamRow][col] = 'X';
+                    Console.WriteLine("Nikoladze killed!");
+                    PrintField();
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- C-Sharp-OOP/01-Working-with-Abstraction/Exercises/06-Sneaking/Startup.cs
+++ C-Sharp-OOP/01-Working-with-Abstraction/Exercises/06-Sneaking/Startup.cs
@@ -1,1 +1,73 @@
+﻿using System;
 
+namespace P06_Sneaking
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            var room = new Room(rows);
+            room.FindSam();
+
+            var directions = Console.ReadLine()
+                .ToCharArray();
+
+            foreach (var direction in directions)
+            {
+                room.MoveEnemies();
+
+                bool samDied = room.CaughtByEnemy();
+
+                if (samDied)
+                {
+                    room.Matrix[room.SamRow][room.SamCol] = 'X';
+                    Console.WriteLine($"Sam died at {room.SamRow}, {room.SamCol}");
+                    room.PrintField();
+                    return;
+                }
+
+                room.Matrix[room.SamRow][room.SamCol] = '.';
+                bool nikoladzeDied;
+
+                switch (direction)
+                {
+                    case 'U':
+                        room.SamRow--;
+                        room.Matrix[room.SamRow][room.SamCol] = 'S';
+                        nikoladzeDied = room.CheckForNikoladze();
+
+                        if (nikoladzeDied)
+                        {
+                            return;
+                        }
+
+                        break;
+
+                    case 'D':
+                        room.SamRow++;
+                        room.Matrix[room.SamRow][room.SamCol] = 'S';
+                        nikoladzeDied = room.CheckForNikoladze();
+
+                        if (nikoladzeDied)
+                        {
+                            return;
+                        }
+
+                        break;
+
+                    case 'L':
+                        room.SamCol--;
+                        room.Matrix[room.SamRow][room.SamCol] = 'S';
+                        break;
+
+                    case 'R':
+                        room.SamCol++;
+                        room.Matrix[room.SamRow][room.SamCol] = 'S';
+                        break;
+                }
+            }
+        }
+    }
+}
