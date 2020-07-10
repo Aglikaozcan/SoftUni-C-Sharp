0500e70ba592bc0bee86a9ea36ab19fca180da22
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:50:44 +0300

    Add files via upload

---
+++ C-Sharp-OOP/01-Working-with-Abstraction/Lab/Engine.cs
@@ -0,0 +1,54 @@
+﻿namespace P03_StudentSystem
+{
+    using P03_StudentSystem.Commands;
+    using P03_StudentSystem.Students;
+    using System;
+
+    public class Engine
+    {
+        private CommandParser commandParser;
+        private StudentSystem studentSystem;
+
+        public Engine(CommandParser commandParser, StudentSystem studentSystem)
+        {
+            this.commandParser = commandParser;
+            this.studentSystem = studentSystem;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    var command = commandParser.Parse(Console.ReadLine());
+
+                    if (command.Name == "Create")
+                    {
+                        var name = command.Arguments[0];
+                        var age = int.Parse(command.Arguments[1]);
+                        var grade = double.Parse(command.Arguments[2]);
+
+                        studentSystem.Add(name, age, grade);
+                    }
+                    else if (command.Name == "Show")
+                    {
+                        var name = command.Arguments[0];
+
+                        var student = studentSystem.Get(name);
+
+                        Console.WriteLine(student);
+                    }
+                    else if (command.Name == "Exit")
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
--- C-Sharp-OOP/01-Working-with-Abstraction/Lab/StartUp.cs
+++ C-Sharp-OOP/01-Working-with-Abstraction/Lab/StartUp.cs
@@ -1,1 +1,17 @@
+﻿namespace P03_StudentSystem
+{
+    using P03_StudentSystem.Commands;
+    using P03_StudentSystem.Students;
 
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var studentSystem = new StudentSystem();
+            var commandParser = new CommandParser();
+            var engine = new Engine(commandParser, studentSystem);
+
+            engine.Run();
+        }
+    }
+}
