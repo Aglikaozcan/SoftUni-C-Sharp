ba3d2b8c29742426abc6d1f373d7a362cd0d0358
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:45:03 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Trade Comissions/Program.cs
+++ C-Sharp Basics/Complex Solutions/Trade Comissions/Program.cs
@@ -1,1 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Trade_Comissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine().ToLower();
+            double sales = double.Parse(Console.ReadLine().ToLower());
+
+            double comission = -1;
+
+            if (town == "sofia")
+            {
+                //0 ≤ s ≤ 500	500 < s ≤ 1 000	1 000 < s ≤ 10 000	s > 10 000
+                //5%	7%	8%	12%
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = 0.05;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = 0.07;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    comission = 0.12;
+                }
+            }
+            else if (town == "varna")
+            {
+                //4.5%	7.5%	10%	13%
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = 0.045;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = 0.075;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = 0.10;
+                }
+                else if (sales > 10000)
+                {
+                    comission = 0.13;
+                }
+            }
+            else if (town == "plovdiv")
+            {
+                //5.5%	8%	12%	14.5%
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = 0.055;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = 0.08;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    comission = 0.145;
+                }
+                
+            }
+            if (comission >= 0)
+            {
+                Console.WriteLine($"{sales * comission:f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
