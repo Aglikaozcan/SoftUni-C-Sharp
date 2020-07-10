b4afb083d16617c049e27e82260330e98d16be8a
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-10 10:31:20 +0300

    Add files via upload

--- C-Sharp Basics/Complex Solutions/Online Education/Program.cs
+++ C-Sharp Basics/Complex Solutions/Online Education/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Online_Education
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formEarly = Console.ReadLine();
+            int studentsEarly = int.Parse(Console.ReadLine());
+            string formRegular = Console.ReadLine();
+            int studentsRegular = int.Parse(Console.ReadLine());
+            string formLate = Console.ReadLine();
+            int studentsLate = int.Parse(Console.ReadLine());
+
+            int totsalStud = studentsEarly + studentsRegular + studentsLate;
+            bool isOnsite = formEarly == "onsite" || formRegular == "onsite" || formLate == "onsite";
+            int studentsOnsite = 0;
+            if (isOnsite)
+            {
+
+            }
+            if (formEarly == "onsite")
+            {
+                studentsEarly = studentsOnsite;
+            }
+
+        }
+    }
+}
