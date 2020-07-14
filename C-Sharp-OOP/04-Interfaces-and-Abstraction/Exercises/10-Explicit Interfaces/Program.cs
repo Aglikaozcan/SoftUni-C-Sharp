d7b14d341796d0d94eee81dfa5d17f26c85da0f4
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-14 13:03:33 +0300

    Add files via upload

--- C-Sharp-OOP/04-Interfaces-and-Abstraction/Exercises/10-Explicit Interfaces/Program.cs
+++ C-Sharp-OOP/04-Interfaces-and-Abstraction/Exercises/10-Explicit Interfaces/Program.cs
@@ -1,1 +1,29 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            while (line != "End")
+            {
+                string[] citizenArgs = line
+                    .Split();
+
+                string name = citizenArgs[0];
+                string country = citizenArgs[1];
+                int age = int.Parse(citizenArgs[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
