e192dd6a51c78b5f5e3b657981b540c5d2bc4015
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-14 14:11:04 +0300

    Add files via upload

--- C-Sharp-OOP/07-Reflection-and-Attributes/Lab/01-Stealer/Program.cs
+++ C-Sharp-OOP/07-Reflection-and-Attributes/Lab/01-Stealer/Program.cs
@@ -1,1 +1,14 @@
+﻿using System;
 
+namespace P01.Stealer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C-Sharp-OOP/07-Reflection-and-Attributes/Lab/01-Stealer/Spy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stealer
+{
+    public class Spy
+    {
+        public void StealFieldInfo(string nameOfClass, string[] fields)
+        {
+
+        }
+    }
+}
