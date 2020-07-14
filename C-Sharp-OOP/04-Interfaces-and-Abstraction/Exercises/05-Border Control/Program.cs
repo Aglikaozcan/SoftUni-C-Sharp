68cf5611f78f461f8ddec01128daaf83f4d4727c
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-14 12:58:44 +0300

    Add files via upload

--- C-Sharp-OOP/04-Interfaces-and-Abstraction/Exercises/05-Border Control/Program.cs
+++ C-Sharp-OOP/04-Interfaces-and-Abstraction/Exercises/05-Border Control/Program.cs
@@ -1,1 +1,47 @@
+﻿using System;
+namespace P05.BorderControl
+{
+    using System.Collections.Generic;
+    using System.Linq;
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<IIdentifiable>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                string id = input[input.Length - 1];
+
+                if (input.Length == 2)
+                {
+                    string model = input[0];
+                    collection.Add(new Robot(model, id));
+                }
+
+                else
+                {
+                    string name = input[0];
+                    int age = int.Parse(input[1]);
+                    collection.Add(new Citizen(name, age, id));
+                }
+            }
+
+            string lastDigits = Console.ReadLine();
+
+            collection
+                .Select(x => x.Id)
+                .Where(x => x.EndsWith(lastDigits))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
