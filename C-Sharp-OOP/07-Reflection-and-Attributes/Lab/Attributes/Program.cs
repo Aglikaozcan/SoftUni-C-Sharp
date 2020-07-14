5a5486c969d577ffce241e222d218a70441eb522
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-14 14:14:00 +0300

    Add files via upload

--- C-Sharp-OOP/07-Reflection-and-Attributes/Lab/Attributes/Program.cs
+++ C-Sharp-OOP/07-Reflection-and-Attributes/Lab/Attributes/Program.cs
@@ -1,1 +1,29 @@
+﻿using System;
 
+namespace Attributes
+{
+    [Flags]
+    public enum Color
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    [Author]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintColor(Color.Blue | Color.Green);
+        }
+
+        public static void PrintColor(Color color)
+        {
+            if (color.HasFlag(Color.Green))
+            {
+
+            }
+        }
+    }    
+}
