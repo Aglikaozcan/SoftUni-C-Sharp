8833037ec8650cd21347cb0cbde7a5350319b592
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-14 13:11:56 +0300

    Add files via upload

---
+++ C-Sharp-OOP/05-Polymorphism/Exercises/03-Wild Farm/Models/Animals/Birds/Hen.cs
@@ -0,0 +1,25 @@
+﻿using P03.WildFarm.Models.Foods;
+using System.Collections.Generic;
+
+namespace P03.WildFarm.Models.Animals.Birds
+{
+    public class Hen : Bird
+    {
+        private const double GainValue = 0.35;
+
+        public Hen(string name, double weight, double wingSize) 
+            : base(name, weight, wingSize)
+        {
+        }
+
+        public override void Eat(Food food)
+        {
+            this.BaseEat(food, new List<string> { nameof(Meat), nameof(Seeds), nameof(Vegetable), nameof(Fruit) }, GainValue);
+        }
+
+        public override string ProduceSound()
+        {
+            return "Cluck";
+        }
+    }
+}
--- C-Sharp-OOP/05-Polymorphism/Exercises/03-Wild Farm/Program.cs
+++ C-Sharp-OOP/05-Polymorphism/Exercises/03-Wild Farm/Program.cs
@@ -1,1 +1,49 @@
+﻿using P03.WildFarm.Models;
+using P03.WildFarm.Models.Animals;
+using P03.WildFarm.Models.Foods;
+using System;
+using System.Collections.Generic;
 
+namespace P03.WildFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Animal> animals = new List<Animal>();
+
+            while (input != "End")
+            {
+                string[] animalArgs = input.Split();
+
+                Animal animal = AnimalFactory.Create(animalArgs);
+
+                string[] foodArgs = Console.ReadLine().Split();
+
+                Food food = FoodFactory.Create(foodArgs);
+
+                Console.WriteLine(animal.ProduceSound());                
+
+                try
+                {
+                    animal.Eat(food);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                animals.Add(animal);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
