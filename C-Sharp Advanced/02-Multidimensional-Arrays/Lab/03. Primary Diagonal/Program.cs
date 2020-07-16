cb699b0f6950b98b769d602906cc3cd08b672be5
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:24:17 +0300

    Add files via upload

--- C-Sharp Advanced/02-Multidimensional-Arrays/Lab/03. Primary Diagonal/Program.cs
+++ C-Sharp Advanced/02-Multidimensional-Arrays/Lab/03. Primary Diagonal/Program.cs
@@ -1,1 +1,46 @@
+﻿using System;
+using System.Linq;
 
+namespace _03._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int startRow = 0;
+            int startCol = 0;
+            int sum = 0;
+
+            while (true)
+            {
+                if (startRow >= n || startCol >= n)
+                {
+                    break;
+                }
+
+                sum += matrix[startRow, startCol];
+                startRow++;
+                startCol++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
