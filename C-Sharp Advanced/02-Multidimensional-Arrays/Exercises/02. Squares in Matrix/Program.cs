b88976213f28455b476a12852b4d30b7924a856d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:29:49 +0300

    Add files via upload

--- C-Sharp Advanced/02-Multidimensional-Arrays/Exercises/02. Squares in Matrix/Program.cs
+++ C-Sharp Advanced/02-Multidimensional-Arrays/Exercises/02. Squares in Matrix/Program.cs
@@ -1,1 +1,48 @@
+﻿using System;
+using System.Linq;
 
+namespace _02.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = Console.ReadLine().Split();
+
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+
+            var matrix = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                char[] currentRow = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = currentRow[j];
+                }
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1]
+                        && matrix[i, j] == matrix[i + 1, j]
+                        && matrix[i, j] == matrix[i + 1, j + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
