9c47f169f6385ac4eb62b62bbec6041a63425367
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:03:52 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Exercises/05. Fashion Boutique/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Exercises/05. Fashion Boutique/Program.cs
@@ -1,1 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _05.FashionBoutique
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] clothesArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> clothes = new Stack<int>(clothesArr);
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int racks = 1;
+
+            while (clothes.Count != 0)
+            {
+                int currentCloth = clothes.Peek();
+
+                if (sum + currentCloth <= rackCapacity)
+                {
+                    sum += currentCloth;
+                    clothes.Pop();
+                }
+                else if (sum + currentCloth > rackCapacity && clothes.Any())
+                {
+                    sum = 0;
+                    racks++;
+                }                
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
