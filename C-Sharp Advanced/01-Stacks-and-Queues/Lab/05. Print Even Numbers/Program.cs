5e766c97f04801c391d5cf26510c3c66cead7a3d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 11:57:20 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Lab/05. Print Even Numbers/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Lab/05. Print Even Numbers/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _05.PrintEvenNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    numbers.Enqueue(input[i]);
+                }
+            }
+
+            var result = numbers.ToList();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
