b749a802df6f68a5eb20fd8fe0b695bb156d2e48
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 11:59:45 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Lab/08. Traffic Jam/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Lab/08. Traffic Jam/Program.cs
@@ -1,1 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _08.TrafficJam
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            int count = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                if (input == "green")
+                {
+                    int carsToPass = Math.Min(number, cars.Count);
+
+                    for (int i = 0; i < carsToPass; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
