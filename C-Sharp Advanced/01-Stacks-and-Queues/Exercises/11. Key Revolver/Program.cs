19b57e640a1b5cccf8cadb13fc581cf271dd1255
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:20:54 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Exercises/11. Key Revolver/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Exercises/11. Key Revolver/Program.cs
@@ -1,1 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _11.KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceBullet = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new Stack<int>(
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            Queue<int> locks = new Queue<int>(
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            int intelligenceValue = int.Parse(Console.ReadLine());
+
+            int bulletCount = 0;
+            int totalBullets = bullets.Count;
+
+            while (bullets.Any() && locks.Any())
+            {
+                var currentBullet = bullets.Peek();
+                var currentLock = locks.Peek();
+
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                bullets.Pop();
+                bulletCount++;
+
+                if (bulletCount == barrelSize)
+                {
+                    Console.WriteLine("Reloading!");
+                    continue;
+                }
+            }
+
+            if (bullets.Count == 0 && locks.Any())
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else if (locks.Count == 0)
+            {
+                int finalPay = intelligenceValue - bulletCount * priceBullet;
+
+                Console.WriteLine($"{totalBullets - bulletCount} bullets left. Earned ${finalPay}");
+            }
+        }
+    }
+}
