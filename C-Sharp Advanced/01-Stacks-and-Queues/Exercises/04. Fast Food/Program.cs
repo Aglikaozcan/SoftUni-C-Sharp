e848d857437da3f9eaa6da4f548032a3b1ccd8e4
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:03:06 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Exercises/04. Fast Food/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Exercises/04. Fast Food/Program.cs
@@ -1,1 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _04.FastFood
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] ordersArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(ordersArr);
+
+            int biggestOrder = orders.Max();
+
+            Console.WriteLine(biggestOrder);
+
+            while (orders.Count != 0)
+            {
+                
+                if (foodQuantity >= orders.Peek())
+                {
+                    int currentOrder = orders.Dequeue();
+
+                    foodQuantity -= currentOrder;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(", ", orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
