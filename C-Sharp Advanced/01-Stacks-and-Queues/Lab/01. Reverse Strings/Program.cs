86d55ef98fce211be290b8c879af13437a6d4007
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 11:51:57 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Lab/01. Reverse Strings/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Lab/01. Reverse Strings/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            var input = Console.ReadLine();
+
+            foreach (var symbol in input)
+            {
+                stack.Push(symbol);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
