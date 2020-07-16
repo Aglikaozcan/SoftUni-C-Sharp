3e18e3b5dc13e013c2d2025ae574cd2b0adf543a
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:25:07 +0300

    Add files via upload

--- C-Sharp Advanced/02-Multidimensional-Arrays/Lab/04. Symbol in Matrix/Program.cs
+++ C-Sharp Advanced/02-Multidimensional-Arrays/Lab/04. Symbol in Matrix/Program.cs
@@ -1,1 +1,50 @@
+﻿using System;
+using System.Linq;
 
+namespace _04._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] currentRow = Console.ReadLine()
+                    .Trim()
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            char findSymbol = char.Parse(Console.ReadLine());
+            int indexRow = 0;
+            int indexCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == findSymbol)
+                    {
+                        indexRow = row;
+                        indexCol = col;
+                        Console.WriteLine($"({indexRow}, {indexCol})");
+                        return;
+                    }                    
+                }
+            }
+
+            if (matrix[indexRow, indexCol] != findSymbol)
+            {
+                Console.WriteLine($"{findSymbol} does not occur in the matrix");
+            }
+        }
+    }
+}
