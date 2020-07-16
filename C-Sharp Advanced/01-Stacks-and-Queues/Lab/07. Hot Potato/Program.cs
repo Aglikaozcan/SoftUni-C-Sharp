d2481d9e51f2ce9e0beebaa6230940b1f037afd5
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 11:58:54 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Lab/07. Hot Potato/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Lab/07. Hot Potato/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _07._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allChildren = Console.ReadLine().Split();
+            Queue<string> children = new Queue<string>(allChildren);
+
+            int number = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while (children.Count > 1)
+            {
+                var currentChild = children.Dequeue();
+
+                if (counter % number != 0)
+                {
+                    children.Enqueue(currentChild);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {currentChild}");
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
