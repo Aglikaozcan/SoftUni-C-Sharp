2af81fe331ba3be37f007f22a3de49152adbf4fc
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 11:58:13 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Lab/06. Supermarket/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Lab/06. Supermarket/Program.cs
@@ -1,1 +1,39 @@
+﻿using System.Collections.Generic;
+using System;
 
+namespace _06.Supermarket
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                if (input == "Paid")
+                {
+                    foreach (var name in names)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    names.Clear();
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
