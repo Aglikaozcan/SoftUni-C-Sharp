84845da34f9a6722f957f913d31a8570e20e1d0d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:23:36 +0300

    Add files via upload

--- C-Sharp Advanced/02-Multidimensional-Arrays/Lab/02. Sum Matrix Columns/Program.cs
+++ C-Sharp Advanced/02-Multidimensional-Arrays/Lab/02. Sum Matrix Columns/Program.cs
@@ -1,1 +1,46 @@
+﻿using System;
+using System.Linq;
 
+namespace _02._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = Console.ReadLine()
+               .Split(", ")
+               .Select(int.Parse)
+               .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            var matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                var sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
