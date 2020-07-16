295c4cd107981aee704739046b9a318b7f3d2f5e
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:33:34 +0300

    Add files via upload

--- C-Sharp Advanced/02-Multidimensional-Arrays/Exercises/07. Knight Game/Program.cs
+++ C-Sharp Advanced/02-Multidimensional-Arrays/Exercises/07. Knight Game/Program.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using System.Linq;
 
+namespace _07.KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            var matrix = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();                 
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, matrix.Select(r => string.Join("", r))));
+        }
+    }
+}
