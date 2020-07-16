79f498cf5f6a280b38ace38618fb6b9bcf76e1b9
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:19:34 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Exercises/09. Simple Text Editor/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Exercises/09. Simple Text Editor/Program.cs
@@ -1,1 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = int.Parse(Console.ReadLine());
+
+            Stack<string> versions = new Stack<string>();
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < numberOfOperations; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string commandName = input[0];
+
+                switch (commandName)
+                {
+                    case "1":
+                        versions.Push(text.ToString());
+                        string textToAdd = input[1];
+                        text.Append(textToAdd);
+                        break;
+                    case "2":
+                        versions.Push(text.ToString());
+                        int removeElementsCount = int.Parse(input[1]);
+                        text.Remove(text.Length - removeElementsCount, removeElementsCount);
+                        break;
+                    case "3":
+                        int index = int.Parse(input[1]) - 1;
+                        Console.WriteLine(text[index]);
+                        break;
+                    case "4":
+                        text.Clear();
+                        text.Append(versions.Pop());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
