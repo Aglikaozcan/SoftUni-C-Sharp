ad45ffcdb685ff36cb90d02e7f3dcfa438cc91c5
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-16 12:18:46 +0300

    Add files via upload

--- C-Sharp Advanced/01-Stacks-and-Queues/Exercises/07. Truck Tour/Program.cs
+++ C-Sharp Advanced/01-Stacks-and-Queues/Exercises/07. Truck Tour/Program.cs
@@ -1,1 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStations = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();            
+
+            for (int i = 0; i < numberOfStations; i++)
+            {
+                int[] stationInfo = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                pumps.Enqueue(stationInfo);
+            }
+
+            int index = 0;
+            int tank = 0;
+
+            while (true)
+            {
+                foreach (var pump in pumps)
+                {
+                    int petrol = pump[0];
+                    int distance = pump[1];
+
+                    tank += petrol - distance;
+
+                    if (tank < 0)
+                    {
+                        var currentPump = pumps.Dequeue();
+                        pumps.Enqueue(currentPump);
+                        index++;
+                    }
+                }
+            }
+
+        }
+    }
+}
