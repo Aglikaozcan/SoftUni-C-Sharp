b3390498c5f1cb3cb9f11a8035c4a8710e112256
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 15:22:42 +0300

    Add files via upload

--- C-Sharp Advanced/09-Iterators-and-Comparators/02. Collection/Program.cs
+++ C-Sharp Advanced/09-Iterators-and-Comparators/02. Collection/Program.cs
@@ -1,1 +1,49 @@
+﻿using System;
+using System.Linq;
 
+namespace P02.Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            ListyIterator<string> listIterator = null;
+
+            while (input != "END")
+            {
+                string[] splitedInput = input.Split();
+
+                string command = splitedInput[0];
+
+                if (command == "Create")
+                {
+                    listIterator = new ListyIterator<string>(splitedInput.Skip(1).ToList());
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listIterator.Move());
+                }
+                else if (command == "Print")
+                {
+                    Console.WriteLine(listIterator.Print());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listIterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    foreach (var element in listIterator)
+                    {
+                        Console.Write(element + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
