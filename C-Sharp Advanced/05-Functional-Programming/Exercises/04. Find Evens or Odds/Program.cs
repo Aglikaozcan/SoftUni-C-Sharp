b5f67c9e379bceb6403159f599344088b639d21e
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:03:19 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Exercises/04. Find Evens or Odds/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Exercises/04. Find Evens or Odds/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rangeArg = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = rangeArg[0];
+            int end = rangeArg[1];
+
+            string typeNumbers = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+
+            Predicate<int> filter = x => typeNumbers == "odd" ? x % 2 != 0 : x % 2 == 0;
+            // Func<int, bool> filterFunc = x => typeNumbers == "odd" ? x % 2 != 0 : x % 2 == 0; //може и така да се зададе
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => filter(x)))); //когато имаме предикат, функцията трябва да се задава така
+
+            // Console.WriteLine(string.Join(" ", numbers.Where(filter))); 
+        }
+    }
+}
