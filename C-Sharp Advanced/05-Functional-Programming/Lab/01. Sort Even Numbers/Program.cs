28ff725ea95853913d1ace8d36839092ec04e6f2
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:55:52 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Lab/01. Sort Even Numbers/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Lab/01. Sort Even Numbers/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Linq;
 
+namespace _01._Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
