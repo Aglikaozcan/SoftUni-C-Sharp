475e5671ae2c6a2b3b81d4ff4a71d4b99d44eafa
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:02:18 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Exercises/03. Custom Min Function/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Exercises/03. Custom Min Function/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Linq;
 
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minNumber = n => n.Min();
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minNumber(numbers));
+        }
+    }
+}
