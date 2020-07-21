56da340d19e4ca1ba40d829c1aec3caa652d7455
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:25:18 +0300

    Add files via upload

---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/09. Rectangle Intersection/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Rectangle_Intersection_2
+{
+    public class Rectangle
+    {
+        public Rectangle(string id, int width, int height, int x, int y)
+        {
+            Id = id;
+            Width = width;
+            Height = height;
+            X = x;
+            Y = y;
+        }
+
+        public string Id { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
