dd911f491098787a6b93824559673b8494adc88f
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:45:55 +0300

    Add files via upload

---
+++ C-Sharp Advanced/08-Generics/Exercises/05. Generic Count Method String/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.GenericCountMethodString
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public List<T> Data => this.data;
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T temp = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- C-Sharp Advanced/08-Generics/Exercises/05. Generic Count Method String/Program.cs
+++ C-Sharp Advanced/08-Generics/Exercises/05. Generic Count Method String/Program.cs
@@ -1,1 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace P05.GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string line = Console.ReadLine();
+                box.Add(line);
+            }
+            
+            string element = Console.ReadLine();
+
+            int count = GetCountOfGreaterElements(box.Data, element);
+
+            Console.WriteLine(count);
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T element)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            T temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
