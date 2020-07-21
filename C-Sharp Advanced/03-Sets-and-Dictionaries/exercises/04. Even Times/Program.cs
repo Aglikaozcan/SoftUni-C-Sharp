5f943c458fcae90c9251b98d571ab22763c2fe6d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:46:43 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/04. Even Times/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/04. Even Times/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers[number] = 0;
+                }
+                numbers[number]++;
+            }
+
+            var evens = numbers.Where(x => x.Value % 2 == 0);
+
+            foreach (var item in evens)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
