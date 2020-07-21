3ab3ab28cb7675345d0eb1d0ede54db39e48fabf
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:30:08 +0300

    Add files via upload

--- C-Sharp Advanced/06-Defining-Classes/Exercises/11. Pokemon Trainer/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/11. Pokemon Trainer/StartUp.cs
@@ -1,1 +1,83 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Tournament")
+                {
+                    break;
+                }
+
+                var tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = tokens[0];
+                string pokemonName = tokens[1];
+                string element = tokens[2];
+                int health = int.Parse(tokens[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, health);
+
+                Trainer trainer = trainers.FirstOrDefault(x => x.Name == trainerName);
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(trainerName);
+                    trainers.Add(trainer);
+                }
+
+                trainer.Pokemons.Add(pokemon);
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Any(x => x.Element == command))
+                    {
+                        trainer.Badges++;
+                    }
+                    else
+                    {
+                        ReduceHealth(trainer.Pokemons);
+                    }
+                }
+            }
+
+            var sortedTrainers = trainers
+                .OrderByDescending(x => x.Badges);
+
+            Console.WriteLine(string.Join(Environment.NewLine, sortedTrainers));
+        }
+
+        static void ReduceHealth(List<Pokemon> pokemons)
+        {
+            for (int i = 0; i < pokemons.Count; i++)
+            {
+                pokemons[i].Health -= 10;
+
+                if (pokemons[i].Health <= 0)
+                {
+                    pokemons.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/11. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Badges} {this.Pokemons.Count}";
+        }
+    }
+}
