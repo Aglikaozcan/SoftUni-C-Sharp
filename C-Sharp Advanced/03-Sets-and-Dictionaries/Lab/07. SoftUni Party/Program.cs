94b3e34d135f26c9664fc9505075627359fc72b2
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:43:26 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/07. SoftUni Party/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/07. SoftUni Party/Program.cs
@@ -1,1 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vip = new HashSet<string>();
+            var regular = new HashSet<string>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "PARTY")
+                {
+                    break;
+                }
+
+                var matchedVipGuest = Regex.Match(line, @"^[\d]\w+$");
+
+                if (matchedVipGuest.Success)
+                {
+                    vip.Add(line);
+                }
+                else
+                {
+                    regular.Add(line);
+                }
+            }
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    Console.WriteLine(vip.Count + regular.Count);
+
+                    foreach (var name in vip)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    foreach (var name in regular)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                    break;
+                }
+
+                if (vip.Contains(line))
+                {
+                    vip.Remove(line);
+                }
+                else if (regular.Contains(line))
+                {
+                    regular.Remove(line);
+                }
+            }
+        }
+    }
+}
