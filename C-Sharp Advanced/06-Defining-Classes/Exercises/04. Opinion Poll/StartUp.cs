dbe37b4491f8d09350424879e32b6e16d746d17b
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:14:26 +0300

    Add files via upload

--- C-Sharp Advanced/06-Defining-Classes/Exercises/04. Opinion Poll/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/04. Opinion Poll/StartUp.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personArgs = Console.ReadLine().Split();
+
+                string name = personArgs[0];
+                int age = int.Parse(personArgs[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            List<Person> peopleOverThirty = family
+                .GetPeopleOverThirty()
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, peopleOverThirty));
+        }
+    }
+}
