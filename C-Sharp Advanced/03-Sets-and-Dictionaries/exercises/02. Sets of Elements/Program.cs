e03a863b8a4c6b7f7229c2c3f4ce40fb581e0f7a
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:45:05 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/02. Sets of Elements/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/02. Sets of Elements/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstSet = lengths[0];
+            int secondSet = lengths[1];
+
+            HashSet<int> firstGroup = new HashSet<int>();
+            HashSet<int> secondGroup = new HashSet<int>();
+
+            for (int i = 1; i <= firstSet + secondSet; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i >= 0 && i <= firstSet)
+                {
+                    firstGroup.Add(number);
+                }
+                else
+                {
+                    secondGroup.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstGroup.Intersect(secondGroup)));
+        }
+    }
+}
