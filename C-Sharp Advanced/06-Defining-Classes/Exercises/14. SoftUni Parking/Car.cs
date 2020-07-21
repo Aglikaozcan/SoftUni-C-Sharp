a12187a07ede7df7fbd133b992efcbcf3c6820ba
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:35:33 +0300

    Add files via upload

---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/14. SoftUni Parking/Car.cs
@@ -0,0 +1,56 @@
+﻿namespace _11.SoftUniParking
+{    
+    using System.Text;
+
+    public class Car
+    {
+        private string make;
+        private string registrationNumber;
+        private int horsePower;
+        private string model;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"HorsePower: {this.HorsePower}");
+            sb.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/14. SoftUni Parking/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _11.SoftUniParking
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
