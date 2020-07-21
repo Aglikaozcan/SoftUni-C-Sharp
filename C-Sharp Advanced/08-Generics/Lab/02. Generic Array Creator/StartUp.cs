3b9632b32a2a38122236666923087245dc2dc130
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:40:16 +0300

    Add files via upload

--- C-Sharp Advanced/08-Generics/Lab/02. Generic Array Creator/StartUp.cs
+++ C-Sharp Advanced/08-Generics/Lab/02. Generic Array Creator/StartUp.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+        }
+    }
+}
