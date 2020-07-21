95961896a9b8e166324254b0009c659b2e7dfc5d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:43:07 +0300

    Add files via upload

--- C-Sharp Advanced/08-Generics/Exercises/02. Generic Box Integer/Program.cs
+++ C-Sharp Advanced/08-Generics/Exercises/02. Generic Box Integer/Program.cs
@@ -1,1 +1,19 @@
+﻿using System;
 
+namespace P02.GenericBoxInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                int content = int.Parse(Console.ReadLine());
+                Box<int> boxWithInt = new Box<int>(content);
+                Console.WriteLine(boxWithInt.ToString());
+            }
+        }
+    }
+}
