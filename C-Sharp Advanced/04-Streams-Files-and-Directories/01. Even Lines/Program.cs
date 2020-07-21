957b05aa055b4f7cc71d9d46f80994075909d750
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:06:49 +0300

    Add files via upload

--- C-Sharp Advanced/04-Streams-Files-and-Directories/01. Even Lines/Program.cs
+++ C-Sharp Advanced/04-Streams-Files-and-Directories/01. Even Lines/Program.cs
@@ -1,1 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
+namespace _01._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string specialSymbols = "-.,!?";
+
+            int counter = 0;
+
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        if (counter % 2 == 0)
+                        {
+                            string changedLine = string.Empty;
+
+                            foreach (var charr in line)
+                            {
+                                if (specialSymbols.Contains(charr))
+                                {
+                                    changedLine += '@';
+                                }
+                                else
+                                {
+                                    changedLine += charr;
+                                }
+                            }
+
+                            string[] splitedLine = changedLine.Split();
+
+                            Array.Reverse(splitedLine);
+
+                            writer.WriteLine(string.Join(" ", splitedLine));
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
