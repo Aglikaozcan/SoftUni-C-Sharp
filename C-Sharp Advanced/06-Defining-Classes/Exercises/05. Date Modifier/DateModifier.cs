0f681828d17ef6c7696108ffbb7c507548fc9a1e
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:19:45 +0300

    Add files via upload

---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05._Date_Modifier
+{
+    public class DateModifier
+    {
+        public double FindDifference(string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            return Math.Abs((dateOne - dateTwo).TotalDays);
+        }
+    }
+}
--- C-Sharp Advanced/06-Defining-Classes/Exercises/05. Date Modifier/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/05. Date Modifier/StartUp.cs
@@ -1,1 +1,17 @@
+﻿using System;
 
+namespace _05._Date_Modifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            var diff = new DateModifier();
+
+            Console.WriteLine(diff.FindDifference(firstDate, secondDate));
+        }
+    }
+}
