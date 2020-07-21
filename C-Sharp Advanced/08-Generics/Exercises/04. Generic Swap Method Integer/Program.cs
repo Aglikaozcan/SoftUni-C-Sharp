be9c6c6c7f9609b5ebcd449361b470a14dd7a3e6
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:45:07 +0300

    Add files via upload

--- C-Sharp Advanced/08-Generics/Exercises/04. Generic Swap Method Integer/Program.cs
+++ C-Sharp Advanced/08-Generics/Exercises/04. Generic Swap Method Integer/Program.cs
@@ -1,1 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.GenericSwapMethodInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                box.Add(line);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            box.Swap(firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+
+        static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            T temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
 
