4f39dac82db14024bbb83dbaf1d82e21329766c0
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:42:47 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/06. Parking Lot/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/06. Parking Lot/Program.cs
@@ -1,1 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> numbers = new HashSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(", ");
+                string command = tokens[0];
+                string plateNumber = tokens[1];
+
+                if (command == "IN")
+                {
+                    numbers.Add(plateNumber);
+                }
+                else
+                {
+                    numbers.Remove(plateNumber);
+                }
+            }
+
+            if (numbers.Count > 0)
+            {
+                foreach (var car in numbers)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }            
+        }
+    }
+}
