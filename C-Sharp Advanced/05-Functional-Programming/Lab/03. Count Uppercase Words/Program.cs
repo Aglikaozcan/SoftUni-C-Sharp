2af4e330f1ef7d9da79fd9be33e0b481c2dbed02
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:57:46 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Lab/03. Count Uppercase Words/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Lab/03. Count Uppercase Words/Program.cs
@@ -1,1 +1,16 @@
+﻿using System;
+using System.Linq;
 
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            => Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => char.IsUpper(w[0]))
+                .ToList()
+                .ForEach(Console.WriteLine);
+    }
+}
