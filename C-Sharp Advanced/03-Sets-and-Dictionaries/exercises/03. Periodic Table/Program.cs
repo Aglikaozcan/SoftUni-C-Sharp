9c23b45c67e6058a7571f38768da8c3f2baaaadf
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:45:49 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/03. Periodic Table/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/03. Periodic Table/Program.cs
@@ -1,1 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] compounds = Console.ReadLine().Split();
+
+                for (int j = 0; j < compounds.Length; j++)
+                {
+                    elements.Add(compounds[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
