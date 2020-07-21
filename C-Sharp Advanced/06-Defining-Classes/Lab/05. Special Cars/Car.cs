12d8d85edf6695ab95322a277c300eb75773f64b
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:10:47 +0300

    Add files via upload

---
+++ C-Sharp Advanced/06-Defining-Classes/Lab/05. Special Cars/Car.cs
@@ -0,0 +1,77 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public static bool Drive(double distance, Car car)
+        {
+            if (car.FuelQuantity - (distance * car.FuelConsumption / 100) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string WhoAmI(Car car)
+        {
+            return $"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nHorsePowers: {car.Engine.HorsePower}\nFuelQuantity: {car.FuelQuantity}";
+        }
+    }
+}
--- C-Sharp Advanced/06-Defining-Classes/Lab/05. Special Cars/Program.cs
+++ C-Sharp Advanced/06-Defining-Classes/Lab/05. Special Cars/Program.cs
@@ -1,1 +1,104 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Collections.Generic;
 
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new List<Tire[]>();
+            var engines = new List<Engine>();
+            var cars = new List<Car>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "No more tires")
+                {
+                    break;
+                }
+
+                var commandArgs = command.Split();
+                int counter = 0;
+                Tire[] tiresSet = new Tire[4];
+
+                for (int i = 0; i < commandArgs.Length - 1; i += 2)
+                {
+                    int year = int.Parse(commandArgs[i]);
+                    double pressure = double.Parse(commandArgs[i + 1]);
+
+                    var currentTire = new Tire(year, pressure);
+
+                    tiresSet[counter] = currentTire;
+                    counter++;
+                }
+
+                tires.Add(tiresSet);
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "Engines done")
+                {
+                    break;
+                }
+
+                int horsePower = int.Parse(input.Split()[0]);
+                double cubicCapacity = double.Parse(input.Split()[1]);
+                var currentEngine = new Engine(horsePower, cubicCapacity);
+
+                engines.Add(currentEngine);
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "Show special")
+                {                  
+                    break;
+                }
+
+                var tokens = input.Split();
+
+                string make = tokens[0];
+                string model = tokens[1];
+                int year = int.Parse(tokens[2]);
+                int fuelQuantity = int.Parse(tokens[3]);
+                double fuelConsumption = double.Parse(tokens[4]);
+                int engineIndex = int.Parse(tokens[5]);
+                int tiresIndex = int.Parse(tokens[6]);
+
+                var currentCar = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tiresIndex]);
+
+                cars.Add(currentCar);
+            }
+
+            foreach (var car in cars)
+            {
+                double pressureSum = 0.0;
+
+                for (int i = 0; i < car.Tires.Length; i++)
+                {
+                    pressureSum += car.Tires[i].Pressure;
+                }
+
+                bool enoughFuel = Car.Drive(20, car);
+
+                if (enoughFuel &&
+                    car.Year >= 2017 &&
+                    car.Engine.HorsePower > 330 &&
+                    pressureSum > 9 && pressureSum < 10)
+                {
+                    car.FuelQuantity -= (20 * car.FuelConsumption / 100);
+
+                    Console.WriteLine(Car.WhoAmI(car));
+                }
+            }
+        }
+    }
+}
