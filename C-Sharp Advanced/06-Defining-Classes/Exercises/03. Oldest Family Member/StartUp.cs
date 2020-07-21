5d553b02a84c813a9020c46734ecfcf9bcb3e2ab
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:13:35 +0300

    Add files via upload

--- C-Sharp Advanced/06-Defining-Classes/Exercises/03. Oldest Family Member/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/03. Oldest Family Member/StartUp.cs
@@ -1,1 +1,31 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
 
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentName = Console.ReadLine().Split();
+
+                string name = currentName[0];
+                int age = int.Parse(currentName[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            Person oldesMember = family.GetOldestMember();
+
+            Console.WriteLine(oldesMember);
+        }
+    }
+}
