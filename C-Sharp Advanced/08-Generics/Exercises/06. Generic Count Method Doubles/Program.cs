dc1f860fef745e53d4ea866d173c32e4b0899017
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:46:54 +0300

    Add files via upload

--- C-Sharp Advanced/08-Generics/Exercises/06. Generic Count Method Doubles/Program.cs
+++ C-Sharp Advanced/08-Generics/Exercises/06. Generic Count Method Doubles/Program.cs
@@ -1,1 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace P06.GenericCountMethodDoubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>();
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                double line = double.Parse(Console.ReadLine());
+                box.Add(line);
+            }
+
+            double element = double.Parse(Console.ReadLine());
+
+            int count = GetCountOfGreaterElements(box.Data, element);
+
+            Console.WriteLine(count);
+        }
+
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T element)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
