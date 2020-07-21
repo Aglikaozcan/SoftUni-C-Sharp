af6ede0854b4362527a31cf2d56c427d3ce9d4d6
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:08:12 +0300

    Add files via upload

--- C-Sharp Advanced/04-Streams-Files-and-Directories/02. Line Numbers/Program.cs
+++ C-Sharp Advanced/04-Streams-Files-and-Directories/02. Line Numbers/Program.cs
@@ -1,1 +1,45 @@
+﻿using System;
+using System.IO;
 
+namespace _02._Line_Numbers_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                int counter = 1;
+
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        int letterCount = 0;
+                        int symbolsCount = 0;
+
+                        foreach (var @char in line)
+                        {
+                            if (char.IsLetter(@char))
+                            {
+                                letterCount++;
+                            }
+                            else if (char.IsPunctuation(@char))
+                            {
+                                symbolsCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {line} ({letterCount})({symbolsCount})");
+
+                        counter++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
