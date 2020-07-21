39d1700662e34efd0e2356fcda0466a094e48726
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:01:43 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Exercises/02. Knights of Honor/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Exercises/02. Knights of Honor/Program.cs
@@ -1,1 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> names = Console.ReadLine()
+            //    .Split()
+            //    .Select(x => $"Sir {x}")
+            //    .ToList();
+
+           // Console.ReadLine()
+           //     .Split()
+           //     .Select(x => $"Sir {x}")
+           //     .ToList()
+           //     .ForEach(Console.WriteLine);
+
+           // Console.ReadLine()
+           //     .Split()
+           //     .ToList()
+           //     .ForEach(x => Console.WriteLine($"Sir {x}"));
+
+            List<string> names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, string> format = x => $"Sir {x}";
+
+            Action<List<string>> print = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(format)));
+
+            print(names);
+        }
+    }
+}
