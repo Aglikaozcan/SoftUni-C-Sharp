a133ef336551d6a1f88ab82d62da1f133253a0ca
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:21:50 +0300

    Add files via upload

---
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/07. Speed Racing/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _07._Speed_Racing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumption = fuelConsumption;
+            this.TraveledDistance = 0;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public int TraveledDistance { get; set; }
+
+        public void CheckFuel(int traveledDistance)
+        {
+            if (FuelAmount - traveledDistance * FuelConsumption >= 0)
+            {
+                FuelAmount -= traveledDistance * FuelConsumption;
+                TraveledDistance += traveledDistance;
+            }
+
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {TraveledDistance}".ToString();
+        }
+    }
+}
--- C-Sharp Advanced/06-Defining-Classes/Exercises/07. Speed Racing/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/07. Speed Racing/StartUp.cs
@@ -1,1 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _07._Speed_Racing
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumption = double.Parse(input[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumption);
+                cars.Add(car);
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+
+                if (command[0] == "End")
+                {
+                    break;
+                }
+
+                string model = command[1];
+                int traveledDistance = int.Parse(command[2]);
+
+                var car = cars.Where(c => c.Model == model).First();
+                car.CheckFuel(traveledDistance);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+    }
+}
