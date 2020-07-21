66d56ff940883bca8f4b208fbb87f45d21583cf8
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:47:47 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/05. Count Symbols/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/exercises/05. Count Symbols/Program.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine()
+                .Trim()
+                .ToCharArray();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+
+                if (symbols.ContainsKey(symbol) == false)
+                {
+                    symbols[symbol] = 0;
+                }
+                symbols[symbol]++;
+            }
+
+            foreach (var symbolKvp in symbols)
+            {
+                Console.WriteLine($"{symbolKvp.Key}: {symbolKvp.Value} time/s");
+            }
+        }
+    }
+}
