f5061dafaca31f87a8ca6c1bfab249193a2ab655
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 11:26:17 +0300

    Add files via upload

--- C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/01. Count Same Values in Array/Program.cs
+++ C-Sharp Advanced/03-Sets-and-Dictionaries/Lab/01. Count Same Values in Array/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> counts = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
