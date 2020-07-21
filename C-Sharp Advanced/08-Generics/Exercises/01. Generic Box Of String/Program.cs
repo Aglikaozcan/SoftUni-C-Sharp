4150ade63b0e9be67954946cf3b2fda973a100a4
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:42:14 +0300

    Add files via upload

--- C-Sharp Advanced/08-Generics/Exercises/01. Generic Box Of String/Program.cs
+++ C-Sharp Advanced/08-Generics/Exercises/01. Generic Box Of String/Program.cs
@@ -1,1 +1,19 @@
+﻿using System;
 
+namespace P01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesCount; i++)
+            {
+                string content = Console.ReadLine();
+                Box<string> boxWithString = new Box<string>(content);
+                Console.WriteLine(boxWithString.ToString());
+            }
+        }
+    }
+}
