fefb7f1161be7dbcf272cc51abd581ff7ae02261
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:08:57 +0300

    Add files via upload

--- C-Sharp Advanced/04-Streams-Files-and-Directories/03. Word Count/Program.cs
+++ C-Sharp Advanced/04-Streams-Files-and-Directories/03. Word Count/Program.cs
@@ -1,1 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            using (var reader = new StreamReader("../../../words.txt"))
+            {
+                while (true)
+                {
+                    var word = reader.ReadLine();
+
+                    if (word == null)
+                    {
+                        break;
+                    }
+
+                    else if (!dictionary.ContainsKey(word))
+                    {
+                        dictionary.Add(word, 0);
+                    }
+                }
+            }
+
+            using (var reader = new StreamReader("../../../words.txt"))
+            {
+                using (var writer = new StreamWriter("../../../actualResult.txt"))
+                {
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            var orderedWords = dictionary.OrderByDescending(w => w.Value);
+
+                            foreach (var word in orderedWords)
+                            {
+                                writer.WriteLine($"{word.Key} - {word.Value}");
+                            }
+
+                            return;
+                        }
+
+                        var words = line
+                            .Split(new char[] { ',', '.', '?', '!', '-', ' ' },
+                            StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (var word in words)
+                        {
+                            if (dictionary.ContainsKey(word.ToLower()))
+                            {
+                                dictionary[word.ToLower()]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
