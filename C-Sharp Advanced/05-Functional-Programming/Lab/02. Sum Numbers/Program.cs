712fa53343069ca5d516fd153e8dea6b862a0f4c
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:56:58 +0300

    Add files via upload

--- C-Sharp Advanced/05-Functional-Programming/Lab/02. Sum Numbers/Program.cs
+++ C-Sharp Advanced/05-Functional-Programming/Lab/02. Sum Numbers/Program.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Linq;
 
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Func<string, int> parser = int.Parse;
+
+            int[] numbers = input
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
