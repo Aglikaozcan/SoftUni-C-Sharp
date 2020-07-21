89576349d29d917b7cbe4f713a02fa7711596cc6
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 12:52:16 +0300

    Add files via upload

--- C-Sharp Advanced/04-Streams-Files-and-Directories/04. Copy Binary File/Program.cs
+++ C-Sharp Advanced/04-Streams-Files-and-Directories/04. Copy Binary File/Program.cs
@@ -1,1 +1,32 @@
+﻿using System;
+using System.IO;
 
+namespace _04._Copy_Binary_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var readFile = new FileStream("../../../copyMe.png", FileMode.Open))
+            {
+                var buffer = new byte[4096];
+
+                using (var writeFile = new FileStream("../../../copyMe-copy.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        int bytesCount = readFile.Read(buffer, 0, buffer.Length);
+
+                        if (bytesCount == 0)
+                        {
+                            break;
+                        }
+
+                        writeFile.Write(buffer, 0, bytesCount);
+                    }
+
+                }
+            }
+        }
+    }
+}
