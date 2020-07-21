ce828a64e1a51bd8b3d42af6dd684c38aacd2beb
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-21 13:31:28 +0300

    Add files via upload

--- C-Sharp Advanced/06-Defining-Classes/Exercises/12. Google/StartUp.cs
+++ C-Sharp Advanced/06-Defining-Classes/Exercises/12. Google/StartUp.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace _11.Google
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
