dc408fed71de156c40498e161e272d9feddbaed7
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-22 14:45:36 +0300

    Add files via upload

--- C-Sharp Advanced/09-Iterators-and-Comparators/03. Stack/Program.cs
+++ C-Sharp Advanced/09-Iterators-and-Comparators/03. Stack/Program.cs
@@ -1,1 +1,53 @@
+﻿using System;
+using System.Linq;
 
+namespace P03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            CustomStack<int> stack = new CustomStack<int>();
+
+            while (input != "END")
+            {
+                string[] splitedInput = input.Split(" ", 2);
+
+                string command = splitedInput[0];
+
+                if (command == "Push")
+                {
+                    int[] numbers = splitedInput[1]
+                        .Split(", ")
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    stack.Push(numbers);
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var number in stack)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
