8732e6c7a4776a5cd51cbc32829bcbfd986cae00
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-23 11:11:42 +0300

    Add files via upload

--- C-Sharp Advanced/Exams/01. Hospital/Program.cs
+++ C-Sharp Advanced/Exams/01. Hospital/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace P01.Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
