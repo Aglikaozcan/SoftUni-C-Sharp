25665c05b329263c196ceb63cdf6f252af59f364
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-23 11:08:38 +0300

    Add files via upload

---
+++ C-Sharp Advanced/Exams/03. Aquarium Adventure/Aquarium.cs
@@ -0,0 +1,68 @@
+﻿namespace AquariumAdventure
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Aquarium
+    {
+        private List<Fish> fishInPool;
+
+        public Aquarium(string name, int capacity, int size)
+        {
+            Name = name;
+            Capacity = capacity;
+            Size = size;
+            fishInPool = new List<Fish>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Size { get; set; }
+
+        public void Add(Fish fish)
+        {
+            if (this.fishInPool.Contains(fish) == false && this.Capacity > this.fishInPool.Count)
+            {
+                this.fishInPool.Add(fish);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var neededFish = fishInPool.FirstOrDefault(x => x.Name == name);
+
+            if (fishInPool.Contains(neededFish))
+            {
+                fishInPool.Remove(neededFish);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public Fish FindFish(string name)
+        {
+            var neededFish = fishInPool.FirstOrDefault(x => x.Name == name);
+
+            return neededFish;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Aquarium: {Name} ^ Size: {Size}");
+
+            foreach (var fish in fishInPool)
+            {
+                sb.AppendLine(fish.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C-Sharp Advanced/Exams/03. Aquarium Adventure/Fish.cs
@@ -0,0 +1,31 @@
+﻿namespace AquariumAdventure
+{
+    using System.Text;
+
+    public class Fish
+    {
+        public Fish(string name, string color, int fins)
+        {
+            Name = name;
+            Color = color;
+            Fins = fins;
+        }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public int Fins { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Fish: {Name}");
+            sb.AppendLine($"Color: {Color}");
+            sb.AppendLine($"Number of fins: {Fins}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
