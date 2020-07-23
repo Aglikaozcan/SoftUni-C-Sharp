5b7eb81c13d78cb758faab50f737c092af44735c
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-23 11:09:43 +0300

    Add files via upload

---
+++ C-Sharp Advanced/Exams/03. Fighting Arena/Arena.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            Name = name;
+            gladiators = new List<Gladiator>();
+        }
+
+        public string Name { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return this.gladiators.Count;
+            }
+        }
+
+        public void Add(Gladiator gladiator)
+        {
+            gladiators.Add(gladiator);
+        }
+
+        public void Remove(string name)
+        {
+            var neededName = gladiators.Where(x => x.Name == name).FirstOrDefault();
+
+            if (gladiators.Contains(neededName))
+            {
+                gladiators.Remove(neededName);
+            }
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            var gladitorWithHighestStatPower = gladiators.OrderByDescending(x => x.GetStatPower()).FirstOrDefault();
+
+            return gladitorWithHighestStatPower;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            var gladitorWithHighestWeaponPower = gladiators.OrderByDescending(x => x.GetWeaponPower()).FirstOrDefault();
+
+            return gladitorWithHighestWeaponPower;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            var gladitorWithHighestTotalPower = gladiators.OrderByDescending(x => x.GetTotalPower()).FirstOrDefault();
+
+            return gladitorWithHighestTotalPower;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Count} gladiators are participating.";
+        }
+    }
+}
---
+++ C-Sharp Advanced/Exams/03. Fighting Arena/Gladiator.cs
@@ -0,0 +1,53 @@
+﻿namespace FightingArena
+{
+    using System.Text;
+
+    public class Gladiator
+    {
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            Name = name;
+            Stat = stat;
+            Weapon = weapon;
+        }
+
+        public string Name { get; set; }
+
+        public Stat Stat { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        public int GetTotalPower()
+        {
+            var sumOfStatProp = this.Stat.Agility + this.Stat.Flexibility + this.Stat.Intelligence + Stat.Skills + Stat.Strength;
+            var sumOfWeaponProps = Weapon.Sharpness + Weapon.Size + Weapon.Solidity;
+
+            return sumOfStatProp + sumOfWeaponProps;
+        }
+
+        public int GetWeaponPower()
+        {
+            var sumOfWeaponProps = Weapon.Sharpness + Weapon.Size + Weapon.Solidity;
+
+            return sumOfWeaponProps;
+        }
+
+        public int GetStatPower()
+        {
+            var sumOfStatProp = this.Stat.Agility + this.Stat.Flexibility + this.Stat.Intelligence + Stat.Skills + Stat.Strength;
+
+            return sumOfStatProp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} - {this.GetTotalPower()}");
+            sb.AppendLine($"  Weapon Power: {this.GetWeaponPower()}");
+            sb.AppendLine($"  Stat Power: {this.GetStatPower()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
