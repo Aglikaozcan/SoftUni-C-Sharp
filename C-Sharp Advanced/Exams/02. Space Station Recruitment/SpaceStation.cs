17e64938637ce8401eee399a62cf872571c2983d
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-23 11:14:59 +0300

    Add files via upload

---
+++ C-Sharp Advanced/Exams/02. Space Station Recruitment/SpaceStation.cs
@@ -0,0 +1,78 @@
+﻿namespace SpaceStationRecruitment
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SpaceStation
+    {
+        private List<Astronaut> data;
+
+        public SpaceStation(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Astronaut>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get { return this.data.Count; }
+        }
+
+        public void Add(Astronaut astronaut)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(astronaut);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            foreach (var astronaut in this.data)
+            {
+                if (astronaut.Name == name)
+                {
+                    this.data.Remove(astronaut);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            var oldestAstronaut = data.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestAstronaut;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            var neededAstronaut = data.FirstOrDefault(x => x.Name == name);
+
+            return neededAstronaut;
+        }        
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Astronauts working at Space Station {this.Name}:");
+
+            foreach (var astronaut in data)
+            {
+                sb.AppendLine(astronaut.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
